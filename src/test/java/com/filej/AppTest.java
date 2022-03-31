3848a98357afd6b2c388043be584d6bf9ea4636b
khanZhomart <jtalgatulu@yandex.kz>
2022-03-31 18:25:16 +0600

    Правка файлов .gitignore и pom.xml

--- src/test/java/com/filej/AppTest.java
+++ src/test/java/com/filej/AppTest.java
@@ -1,20 +1,56 @@
 package com.filej;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThrows;
 
+import com.filej.commands.Command;
+import com.filej.commands.filecommands.DeleteFileCommand;
+import com.filej.commands.filecommands.TouchFileCommand;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+
+public class AppTest {
+    private static final Logger logger = LoggerFactory.getLogger(AppTest.class);
+    private Command command;
+    private final String testFile = "test.txt";
+
+    @Before
+    public void init() {
+        command = new TouchFileCommand(false, testFile);
+
+        try {
+            command.run();
+            logger.info("created " + testFile + " file.");
+        } catch (Exception e) {
+            logger.warn(testFile + " file was already created. Skipping...");
+        }
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void Should_ReturnIllegalArgumentException_When_ProvidedFilenameExists() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {
+                command = new TouchFileCommand(false, testFile);
+                logger.info("trying to create duplicate file " + testFile + "...");
+                command.run();
+            }
+        );
+    }
+
+    @After
+    public void after() {
+        command = new DeleteFileCommand(false, true, testFile);
+        
+        try {
+            command.run();
+            logger.info("deleted file " + testFile);
+        } catch (Exception e) {
+            logger.error("something gone wrong...");
+        }
     }
 }
