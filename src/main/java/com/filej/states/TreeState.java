bfa875a69e3ad317c5200c25bcecdd55afe5c2d8
khanZhomart <jtalgatulu@yandex.kz>
2022-04-04 09:58:36 +0600

    Оптимизация кода

--- src/main/java/com/filej/states/TreeState.java
+++ src/main/java/com/filej/states/TreeState.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public enum TreeState {
     INSTANCE;
 
-    public static final Logger logger = LoggerFactory.getLogger(TreeState.class);
     private static List<String> pseudoPath;
     private static String realPath;
 
